2eed0f87f1292eb08ecd7589feedf582e9945c47
arun <arunks002info@gmail.com>
2024-11-20 22:21:34 +0530

    check auth-routing-enclosed

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import AdminDashboard from "./pages/admin-view/dashboard";
 import AdminProducts from "./pages/admin-view/products";
 import AdminOrders from "./pages/admin-view/orders";
 import AdminFeatures from "./pages/admin-view/features";
-import ShoppingHeader from "./components/shopping-view/header";
+//import ShoppingHeader from "./components/shopping-view/header";
 import ShoppingLayout from "./components/shopping-view/layout";
 import NotFound from "./pages/not-found";
 import ShoppingHome from "./pages/shopping-view/home";
@@ -20,7 +20,8 @@ import ShoppingAccount from "./pages/shopping-view/account";
 // import ShoppingListing from "./pages/shopping-view/listing";
 // import ShoppingCheckout from "./pages/shopping-view/checkout";
 // import ShoppingAccount from "./pages/shopping-view/account";
-// import CheckAuth from "./components/common/check-auth";
+import CheckAuth from "./components/common/check-auth";
+import UnauthPage from "./pages/unauth-page";
 //import UnauthPage from "./pages/unauth-page";
 // import { useDispatch, useSelector } from "react-redux";
 // import { useEffect } from "react";
@@ -31,26 +32,51 @@ import ShoppingAccount from "./pages/shopping-view/account";
 // import SearchProducts from "./pages/shopping-view/search";
 
 function App() {
+  const isAuthenticated = false;
+  const user = null;
   return (
     <div className="flex flex-col overflow-hidden bg-white">
       <Routes>
-        <Route path="/auth" element={<AuthLayout />}>
+        <Route
+          path="/auth"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AuthLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="login" element={<AuthLogin />} />
           <Route path="register" element={<AuthRegister />} />
         </Route>
-        <Route path="/admin" element={<AdminLayout />}>
+        <Route
+          path="/admin"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AdminLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="dashboard" element={<AdminDashboard />} />
           <Route path="products" element={<AdminProducts />} />
           <Route path="orders" element={<AdminOrders />} />
           <Route path="features" element={<AdminFeatures />} />
         </Route>
-        <Route path="/shop" element={<ShoppingLayout />}>
+        <Route
+          path="/shop"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <ShoppingLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="home" element={<ShoppingHome />} />
           <Route path="listing" element={<ShoppingListing />} />
           <Route path="checkout" element={<ShoppingCheckout />} />
           <Route path="account" element={<ShoppingAccount />} />
         </Route>
+
         <Route path="*" element={<NotFound />} />
+        <Route path="unauth-page" element={<UnauthPage />} />
       </Routes>
     </div>
   );
